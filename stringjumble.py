d81fc015d0ec7bd65b9b9e22dffc76ada02bac4a
kylerozzi <42872056+kylerozzi@users.noreply.github.com>
2018-09-28 12:03:01 -0400

    Updated from Brython Server: 9/28/2018 12:03:17 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
 Author: <your name>
-Credit: <sources>
+Credit: https://www.quora.com/How-can-I-reverse-a-list-in-python   https://www.geeksforgeeks.org/join-function-python/
 
 Assignment:
 
@@ -25,3 +25,14 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+v1=str(input("Please enter a string of text (the bigger the better): "))
+print('You entered "'+v1+'". Now jumble it: ')
+print(v1[::-1])
+
+v2=v1.split()
+v2.reverse()
+v2v=" "
+v2v=v2v.join(v2)
+print(v2v)
+print(v2v[::-1])
+
